e0cfc1d300007f62a509b72db31e513a6fa048d2
Ellevern <elisavandk@gmail.com>
2025-05-16 19:30:29 -0400

    Filled scripts from subproject code. Note: This code is original. It was tested and completed in a separate project called GunMechanics

--- Assets/Scripts/XRMagazineWell.cs
+++ Assets/Scripts/XRMagazineWell.cs
@@ -1,16 +1,106 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class XRMagazineWell : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Transform attachPoint;
+    [SerializeField]
+    private XRMagazine activeMagazine;
+
+    public UnityAction onMagazineReleased;
+    public UnityAction onMagazineInsert;
+    private Animator animator;
+    private bool isMagazineAttached = false;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (activeMagazine != null)
+        {
+            AttachMagazine();
+        }
+    }
+
+    private void Update()
+    {
+        if (activeMagazine != null && !isMagazineAttached)
+        {
+            AttachMagazine();
+        }
+    }
+
+    // Attach Magazine to attach point and play animation
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activeMagazine != null) return;
+
+        XRMagazine magazine;
+
+        if (other.transform.parent != null && other.transform.parent.TryGetComponent<XRMagazine>(out magazine))
+        {
+            if (!magazine.IsHeld()) return;
+            activeMagazine = magazine;
+            activeMagazine.ForceDetach();
+        }
+    }
+
+    private void AttachMagazine()
+    {
+        isMagazineAttached = activeMagazine.transform.parent == attachPoint;
+        if (isMagazineAttached)
+        {
+            animator.SetBool("HasMag", true);
+            return;
+        }
+        activeMagazine.transform.parent = attachPoint;
+        activeMagazine.transform.localPosition = Vector3.zero;
+        activeMagazine.transform.localRotation = Quaternion.identity;
+        activeMagazine.LockInteraction();
+    }
+
+    // Call after release magazine animation completes
+    public void MagazineReleased()
+    {
+        activeMagazine.transform.parent = null;
+        activeMagazine.UnlockInteraction();
+        activeMagazine = null;
+        isMagazineAttached = false;
+        onMagazineReleased?.Invoke();
+    }
+
+    // Call after insert magazine animation completess
+    public void MagazineInsert()
+    {
+        onMagazineInsert?.Invoke();
+    }
+
+    // Call from Pistol Class
+    public void ReleaseMagazine()
+    {
+        if (activeMagazine == null) return;
+        animator.SetBool("HasMag", false);
+    }
+
+    public bool HasLoadedMagazine()
+    {
+        return activeMagazine != null && activeMagazine.CurrentAmmo > 0;
+    }
+
+    public int GetAmmoInMag()
+    {
+        if (activeMagazine == null) return 0;
+
+        return (int)activeMagazine.CurrentAmmo;
+    }
+
+    public void SetAmmoInMag(int amount)
+    {
+        if (activeMagazine == null) return;
+        activeMagazine.CurrentAmmo = amount;
     }
 }
--- Assets/Scripts/XRPistol.cs
+++ Assets/Scripts/XRPistol.cs
@@ -1,16 +1,251 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class XRPistol : MonoBehaviour
+public class XRPistol : XRGrabInteractable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private InputAction rightReleaseMagAction;
+    [SerializeField]
+    private InputAction leftReleaseMagAction;
+
+    [SerializeField]
+    private Transform shootingOrigin;
+    [SerializeField]
+    private Transform ejectOrigin;
+    [SerializeField]
+    private GameObject casingPrefab;
+
+    [SerializeField]
+    [Range(0f, 3f)]
+    private float minShotPitch = 0.6f;
+    [SerializeField]
+    [Range(0f, 3f)]
+    private float maxShotPitch = 1.2f;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float minShotVolume = 0.8f;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float maxShotVolume = 1.0f;
+
+    [SerializeField]
+    private ParticleSystem smokeParticles;
+    [SerializeField]
+    private ParticleSystem impactSparks;
+
+    [SerializeField]
+    private AudioClip shootClip;
+    [SerializeField]
+    private AudioClip emptyClip;
+    [SerializeField]
+    private AudioClip pulledBackClip;
+    [SerializeField]
+    private AudioClip snappedForwardClip;
+
+    [SerializeField]
+    private XRPistolSlider slider;
+    [SerializeField]
+    private XRMagazineWell magWell;
+
+    [Tooltip("Locks the slider when the pistol is not held")]
+    [SerializeField]
+    private bool lockSlideWhenNotHeld;
+
+    private Animator animator;
+    private AudioSource audioSource;
+    private bool animationPlaying = false;
+    private int currentAmmo;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        DeterminePistolState();
+
+        if (lockSlideWhenNotHeld)
+        {
+            slider.LockSlideForAnimation();
+        }
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        slider.onSnapForward += OnSlideSnappedForward;
+        slider.onPullBack += OnSlidePulledBack;
+        magWell.onMagazineInsert += MagazineAttached;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        rightReleaseMagAction.Disable();
+        rightReleaseMagAction.performed -= OnReleaseMagazinePressed;
+        leftReleaseMagAction.Disable();
+        leftReleaseMagAction.performed -= OnReleaseMagazinePressed;
+        slider.onSnapForward -= OnSlideSnappedForward;
+        slider.onPullBack -= OnSlidePulledBack;
+        magWell.onMagazineInsert -= MagazineAttached;
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+
+        XRDirectInteractor interactor = args.interactorObject as XRDirectInteractor;
+
+        if (interactor != null)
+        {
+            switch (interactor.handedness)
+            {
+                case InteractorHandedness.Left:
+                    // Subscribe to left hand
+                    leftReleaseMagAction.Enable();
+                    leftReleaseMagAction.performed += OnReleaseMagazinePressed;
+                    break;
+                case InteractorHandedness.Right:
+                    // Subscribe to right hand
+                    rightReleaseMagAction.Enable();
+                    rightReleaseMagAction.performed += OnReleaseMagazinePressed;
+                    break;
+            }
+
+            if (lockSlideWhenNotHeld)
+            {
+                slider.UnlockSlide();
+            }
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        rightReleaseMagAction.Disable();
+        rightReleaseMagAction.performed -= OnReleaseMagazinePressed;
+        leftReleaseMagAction.Disable();
+        leftReleaseMagAction.performed -= OnReleaseMagazinePressed;
+
+        if (lockSlideWhenNotHeld)
+        {
+            slider.LockSlideForAnimation();
+        }
+    }
+
+    public void PullTrigger()
+    {
+        if (animationPlaying) return;
+
+        if (currentAmmo > 0 && slider.SliderIsIdle())
+        {
+            animationPlaying = true;
+            slider.LockSlideForAnimation();
+            animator.SetTrigger(currentAmmo == 1 ? "ShootLast" : "Shoot");
+        }
+        else
+        {
+            PlayAudioClip(emptyClip);
+        }
+    }
+
+    // TODO: Add haptic feedback on shoot
+    public void ShootPistol()
+    {
+        currentAmmo--;
+        magWell.SetAmmoInMag(currentAmmo);
+        PlayAudioClip(shootClip);
+        // PlaySmokeEffect();
+        RaycastHit hit;
+
+        if (Physics.Raycast(shootingOrigin.position, shootingOrigin.forward, out hit))
+        {
+            PlayImpactSparks(hit.point, hit.collider.transform.rotation);
+        }
+    }
+
+    public void EjectCasing()
+    {
+        // TODO: Implement casing ejection
+    }
+
+    private void PlayAudioClip(AudioClip clip)
+    {
+        if (audioSource == null) return;
+
+        float pitch = Random.Range(minShotPitch, maxShotPitch);
+        float volume = Random.Range(minShotVolume, maxShotVolume);
+        audioSource.pitch = pitch;
+        audioSource.volume = volume;
+
+        audioSource.PlayOneShot(clip);
+    }
+
+    private void PlaySmokeEffect()
+    {
+        ParticleSystem smoke = Instantiate(smokeParticles, shootingOrigin.position, shootingOrigin.rotation);
+    }
+
+    private void PlayImpactSparks(Vector3 impactPoint, Quaternion impactRotation)
+    {
+        ParticleSystem sparks = Instantiate(impactSparks, impactPoint, impactRotation);
+    }
+
+    public void SetPistolAnimationEnd()
+    {
+        animationPlaying = false;
+        DeterminePistolState();
+    }
+
+    private void DeterminePistolState()
+    {
+        if (currentAmmo == 0)
+        {
+            slider.SetEmptyState(true);
+            animator.SetBool("ShotReady", false);
+        }
+    }
+
+    private void MagazineAttached()
+    {
+        slider.SetEmptyState(false);
+    }
+
+    private void OnReleaseMagazinePressed(InputAction.CallbackContext ctx)
+    {
+        // Leave one round in chamber if loaded
+        int tempAmmo = currentAmmo;
+        currentAmmo = currentAmmo > 1 ? 1 : 0;
+        magWell.SetAmmoInMag(tempAmmo - currentAmmo);
+        magWell.ReleaseMagazine();
+    }
+
+    private void OnSlidePulledBack()
+    {
+        PlayAudioClip(pulledBackClip);
+        // TODO: Eject round when in chamber
+        if (currentAmmo > 0)
+        {
+            currentAmmo--;
+            EjectCasing();
+            DeterminePistolState();
+        }
+    }
+
+    private void OnSlideSnappedForward()
+    {
+        PlayAudioClip(snappedForwardClip);
+
+        if (currentAmmo == 0 && magWell.HasLoadedMagazine())
+        {
+            currentAmmo = magWell.GetAmmoInMag();
+            animator.SetBool("ShotReady", true);
+        }
     }
 }
