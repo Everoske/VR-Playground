16791e3acea2e563741055b99fc16b3bc3e5d56b
Ellevern <elisavandk@gmail.com>
2025-06-09 19:41:46 -0400

    Started implementing Round Set logic

--- Assets/Scripts/Game/RoundSet.cs
+++ Assets/Scripts/Game/RoundSet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace ShootingGallery.Game
 {
@@ -13,6 +14,63 @@ namespace ShootingGallery.Game
 
         [SerializeField]
         private float timeBeforeSet = 1.0f; // Time before set
+
+        private int totalPointsEarned = 0;
+        private int targetSetsComplete = 0;
+
+        public UnityAction<int> onRoundSetComplete; // Inform GalleryRound with total points
+
+        private void OnEnable()
+        {
+            SubscribeToTargetSets();
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeFromTargetSets();
+        }
+
+        private void ShootingTargetHit(int points)
+        {
+            totalPointsEarned = Mathf.Max(totalPointsEarned + points, 0);
+        }
+
+        private void TargetSetComplete()
+        {
+            targetSetsComplete++;
+            if (targetSetsComplete >= targetSets.Length)
+            {
+                onRoundSetComplete?.Invoke(totalPointsEarned);
+            }
+        }
+
+        // Stop any target set currently active
+        // Target sets should call onSetComplete when they are out of view from player
+        private void StopTargetSets()
+        {
+            foreach (TargetSet targetSet in targetSets)
+            {
+                //targetSet.StopTargetSet();
+            }
+        }
+
+        private void SubscribeToTargetSets()
+        {
+            foreach (TargetSet targetSet in targetSets)
+            {
+                targetSet.onTargetHit += ShootingTargetHit;
+                targetSet.onTargetSetComplete += TargetSetComplete;
+            }
+        }
+
+        private void UnsubscribeFromTargetSets()
+        {
+            foreach (TargetSet targetSet in targetSets)
+            {
+                targetSet.onTargetHit -= ShootingTargetHit;
+                targetSet.onTargetSetComplete -= TargetSetComplete;
+            }
+        }
 
         public float TimeBeforeSet
         {
