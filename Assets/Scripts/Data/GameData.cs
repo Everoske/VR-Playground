7914bd8df2834ae5a396f1b4becaa33b7025bad4
Ellevern <elisavandk@gmail.com>
2025-06-20 01:10:36 -0400

    Added GameData, SaveDataManager, ISaveable, and DataFileHandler classes.

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ShootingGallery.Data
+{
+    public class GameData
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interfaces/ISaveable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ShootingGallery.Interfaces
+{
+    public interface ISaveable
+    {
+
+    }
+}
