dc5ba1bf741307815a92410e2311ae021114bca7
Ellevern <elisavandk@gmail.com>
2025-06-29 04:22:05 -0400

    Added fade in/fade out to hand menu.

--- Assets/Scripts/UI/Hand Menu/HandMenu.cs
+++ Assets/Scripts/UI/Hand Menu/HandMenu.cs
@@ -18,6 +18,7 @@ namespace ShootingGallery.UI.HandMenu
 
         private bool isOpen;
         private bool shouldFadeIn = false;
+        private bool shouldFadeOut = false;
         private float fadeCounter = 0.0f;
         private CanvasGroup alphaController;
 
@@ -32,18 +33,87 @@ namespace ShootingGallery.UI.HandMenu
             gameObject.SetActive(false);
         }
 
+        private void Update()
+        {
+            HandleFade();
+        }
+
         public void OpenHandMenu()
         {
             if (isOpen) return;
             gameObject.SetActive(true);
+            shouldFadeOut = false;
             isOpen = true;
+
+            if (alphaController.alpha > 0.0f)
+            {
+                fadeCounter = (1 - alphaController.alpha / 1) * fadeTime;
+            }
+            else
+            {
+                fadeCounter = 0.0f;
+            }
+
+            shouldFadeIn = true;
         }
 
         public void CloseHandMenu()
         {
             if (!isOpen) return;
-            gameObject.SetActive(false);
             isOpen = false;
+            shouldFadeIn = false;
+
+            if (alphaController.alpha < 1.0f)
+            {
+                fadeCounter = (alphaController.alpha / 1) * fadeTime;
+            }
+            else
+            {
+                fadeCounter = 0;
+            }
+
+            shouldFadeOut = true;
+        }
+
+        private void HandleFade()
+        {
+            if (!shouldFadeIn && !shouldFadeOut) return;
+
+            fadeCounter += Time.deltaTime;
+
+            if (shouldFadeIn)
+            {
+                FadeIn();
+            }
+            else
+            {
+                FadeOut();
+            }
+        }
+
+        private void FadeIn()
+        {
+            if (fadeCounter < fadeTime)
+            {
+                alphaController.alpha = Mathf.Lerp(0.0f, 1.0f, fadeCounter / fadeTime);
+                return;
+            }
+
+            alphaController.alpha = 1.0f;
+            shouldFadeIn = false;
+        }
+
+        private void FadeOut()
+        {
+            if (fadeCounter < fadeTime)
+            {
+                alphaController.alpha = Mathf.Lerp(1.0f, 0.0f, fadeCounter / fadeTime);
+                return;
+            }
+
+            alphaController.alpha = 0.0f;
+            shouldFadeOut = false;
+            gameObject.SetActive(false);
         }
     }
 }
