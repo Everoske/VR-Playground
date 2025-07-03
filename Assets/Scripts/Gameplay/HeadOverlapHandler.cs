998eaad4a9c7f3463676543b7ec55acc6a604976
Ellevern <elisavandk@gmail.com>
2025-07-03 09:55:28 -0400

    Added scene/script management.

--- Assets/Scripts/Gameplay/HeadOverlapHandler.cs
+++ Assets/Scripts/Gameplay/HeadOverlapHandler.cs
@@ -8,13 +8,16 @@ namespace ShootingGallery.Gameplay
         private LayerMask overlapLayerMask;
 
         [SerializeField]
+        private float overlapRadius = 0.25f;
+
+        [SerializeField]
         private Renderer fadeRenderer;
 
         private void Update()
         {
             int maxColliders = 10;
             Collider[] hitColliders = new Collider[maxColliders];
-            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, 0.3f, hitColliders, overlapLayerMask);
+            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, overlapRadius, hitColliders, overlapLayerMask);
             fadeRenderer.enabled = numColliders > 0;
         }
     }
