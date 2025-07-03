24916599486a2a193090594cfdb71530df9767e5
Ellevern <elisavandk@gmail.com>
2025-07-03 07:55:58 -0400

    Implemented respawning XRPistol when out of bounds. Ensured pistol respawns with no momentum.

--- Assets/Scripts/Gameplay/WeaponRestrictedVolume.cs
+++ Assets/Scripts/Gameplay/WeaponRestrictedVolume.cs
@@ -7,20 +7,40 @@ namespace ShootingGallery.Gameplay
     {
         [SerializeField]
         private Transform destination;
+        [SerializeField]
+        private int maxParentsToCheck = 5;
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.TryGetComponent<XRPistol>(out XRPistol weapon))
-            {
-                TeleportWeapon(weapon);
-            }
+            FindXRWeaponInParent(1, other.gameObject);
         }
 
         private void TeleportWeapon(XRPistol weapon)
         {
             weapon.gameObject.SetActive(false);
             weapon.gameObject.transform.position = destination.position;
+            
+            if (weapon.gameObject.TryGetComponent<Rigidbody>(out Rigidbody weaponRB))
+            {
+                weaponRB.angularVelocity = Vector3.zero;
+                weaponRB.linearVelocity = Vector3.zero;
+            }
+
             weapon.gameObject.SetActive(true);
+        }
+
+        private void FindXRWeaponInParent(int depth, GameObject current)
+        {
+            if (depth > maxParentsToCheck) return;
+
+            if (current.TryGetComponent<XRPistol>(out XRPistol weapon))
+            {
+                TeleportWeapon(weapon);
+                return;
+            }
+
+            if (current.transform.parent == null) return;
+            FindXRWeaponInParent(depth + 1, current.transform.parent.gameObject);
         }
     }
 }
