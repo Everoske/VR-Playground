14e7275001502447b6ab7f1f280696deca5851e7
Ellevern <elisavandk@gmail.com>
2025-07-03 09:38:22 -0400

    Created basic, rough, implemention of blacking out screen when player in collision.

--- Assets/Scripts/Gameplay/HeadOverlapHandler.cs
+++ Assets/Scripts/Gameplay/HeadOverlapHandler.cs
@@ -4,6 +4,18 @@ namespace ShootingGallery.Gameplay
 {
     public class HeadOverlapHandler : MonoBehaviour
     {
+        [SerializeField]
+        private LayerMask overlapLayerMask;
 
+        [SerializeField]
+        private Renderer fadeRenderer;
+
+        private void Update()
+        {
+            int maxColliders = 10;
+            Collider[] hitColliders = new Collider[maxColliders];
+            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, 0.3f, hitColliders, overlapLayerMask);
+            fadeRenderer.enabled = numColliders > 0;
+        }
     }
 }
