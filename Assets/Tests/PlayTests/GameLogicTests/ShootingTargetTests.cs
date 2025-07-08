828a2f8fdd2403f593dbf0edea0f532fd87518b3
Ellevern <elisavandk@gmail.com>
2025-07-08 19:20:55 -0400

    Added Verify Shooting Target Rotation On Hit Test.

--- Assets/Tests/PlayTests/GameLogicTests/ShootingTargetTests.cs
+++ Assets/Tests/PlayTests/GameLogicTests/ShootingTargetTests.cs
@@ -17,18 +17,30 @@ public class ShootingTargetTests
     }
 
     [UnityTest]
-    public IEnumerator VerifyShootingTargetMaterialChange()
+    public IEnumerator VerifyShootingTargetMaterialChangeOnHit()
     {
         yield return WaitForSceneLoad();
 
         var gameObject = GameObject.Find("Shooting Target");
         ShootingTarget target = gameObject.GetComponent<ShootingTarget>();
-        MeshRenderer targetMesh = target.GetComponentInChildren<MeshRenderer>();
-        Material activeMaterial = targetMesh.material;
+        MeshRenderer meshRenderer = target.GetComponentInChildren<MeshRenderer>();
+        Material activeMaterial = meshRenderer.material;
         target.HitTarget();
-        Material inactiveMaterial = targetMesh.material;
+        Material inactiveMaterial = meshRenderer.material;
 
-        Assert.AreNotEqual(activeMaterial, inactiveMaterial);
+        Assert.AreNotEqual(activeMaterial, inactiveMaterial, "Shooting Target should change material on hit");
+    }
+
+    [UnityTest]
+    public IEnumerator VerifyShootingTargetRotationOnHit()
+    {
+        yield return WaitForSceneLoad();
+        var gameObject = GameObject.Find("Shooting Target");
+        ShootingTarget target = gameObject.GetComponent<ShootingTarget>();
+        target.HitTarget();
+
+        yield return new WaitForSeconds(0.5f);
+        Assert.AreEqual(270.0f, target.transform.eulerAngles.x, "Shooting Target should rotate on hit");
     }
 
     private IEnumerator WaitForSceneLoad()
