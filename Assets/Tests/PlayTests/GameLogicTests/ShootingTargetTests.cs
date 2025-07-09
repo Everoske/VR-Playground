0b334b1b97ef8fa6927db2c161d519afd60cd5cf
Ellevern <elisavandk@gmail.com>
2025-07-09 16:35:37 -0400

    Added tests for reseting shooting target.

--- Assets/Tests/PlayTests/GameLogicTests/ShootingTargetTests.cs
+++ Assets/Tests/PlayTests/GameLogicTests/ShootingTargetTests.cs
@@ -9,6 +9,8 @@ using ShootingGallery.Game;
 public class ShootingTargetTests
 {
     private readonly string scenePath = "Assets/Scenes/TestingScene.unity";
+    private readonly float hitRotation = 270.0f;
+    private readonly float defaultRotation = 0.0f;
 
     [OneTimeSetUp]
     public void LoadScene()
@@ -28,7 +30,7 @@ public class ShootingTargetTests
         target.HitTarget();
         Material inactiveMaterial = meshRenderer.material;
 
-        Assert.AreNotEqual(activeMaterial, inactiveMaterial, "Shooting Target should change material on hit");
+        Assert.AreNotEqual(activeMaterial.name, inactiveMaterial.name, "Shooting Target should change material on hit");
     }
 
     [UnityTest]
@@ -40,7 +42,44 @@ public class ShootingTargetTests
         target.HitTarget();
 
         yield return new WaitForSeconds(0.5f);
-        Assert.AreEqual(270.0f, target.transform.eulerAngles.x, "Shooting Target should rotate on hit");
+        Assert.AreEqual(hitRotation, target.transform.eulerAngles.x, "Shooting Target should rotate on hit");
+    }
+
+    [UnityTest]
+    public IEnumerator VerifyShootingTargetResetMaterial()
+    {
+        yield return WaitForSceneLoad();
+        var gameObject = GameObject.Find("Shooting Target");
+        ShootingTarget target = gameObject.GetComponent<ShootingTarget>();
+        MeshRenderer meshRenderer = target.GetComponentInChildren<MeshRenderer>();
+        Material activeMaterial = meshRenderer.material;
+        target.HitTarget();
+
+        yield return null;
+        target.ResetTarget();
+        Material resetMaterial = meshRenderer.material;
+        Assert.AreEqual(activeMaterial.name, resetMaterial.name, "Should reset material on reset target");
+    }
+
+    [UnityTest]
+    public IEnumerator VerifyShootingTargetRotationReset()
+    {
+        yield return WaitForSceneLoad();
+        var gameObject = GameObject.Find("Shooting Target");
+        ShootingTarget target = gameObject.GetComponent<ShootingTarget>();
+        target.HitTarget();
+
+        yield return null;
+        target.ResetTarget();
+        Assert.AreEqual(defaultRotation, target.transform.eulerAngles.x, "Should reset rotation on reset target");
+    }
+
+    [TearDown]
+    public void ResetShootingTarget()
+    {
+        var gameObject = GameObject.Find("Shooting Target");
+        ShootingTarget target = gameObject.GetComponent<ShootingTarget>();
+        target.ResetTarget();
     }
 
     private IEnumerator WaitForSceneLoad()
