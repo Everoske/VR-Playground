f0e67ebf7b99e5cbd6a9db05e0ffcb9ce8878f94
Ellevern <elisavandk@gmail.com>
2025-08-27 20:23:35 -0400

    Implemented ammo spawning with XR Ammo Volume.

--- Assets/Scripts/XR/Weapon/XRAmmoVolume.cs
+++ Assets/Scripts/XR/Weapon/XRAmmoVolume.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 namespace ShootingGallery.XR
 {
     /// <summary>
     /// An interactable volume that infinitely spawns magazines.
     /// </summary>
-    public class XRAmmoVolume : MonoBehaviour
+    public class XRAmmoVolume : XRBaseInteractable
     {
-        
+        [SerializeField]
+        private XRGrabInteractable ammoInterablePrefab;
+
+        protected override void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            base.OnSelectEntered(args);
+            XRDirectInteractor selectInteractor = args.interactorObject as XRDirectInteractor;
+
+            if (selectInteractor != null)
+            {
+                XRGrabInteractable ammoInteractable = Instantiate(ammoInterablePrefab, transform.position, Quaternion.identity);
+                interactionManager.SelectEnter((IXRSelectInteractor) selectInteractor, (IXRSelectInteractable) ammoInteractable);
+            }
+        }
     }
 }
